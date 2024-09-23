03effceb3fb68aa5631d9cea9a441d9c80d691fe
AryaJha03 <aryanandanjha@gmail.com>
2024-09-23 05:34:04 +0530

    init push

---
+++ src/main/java/com/sheryians/major/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.sheryians.major.service;
+
+import com.sheryians.major.model.Category;
+import com.sheryians.major.model.Product;
+import com.sheryians.major.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> getAllProduct(){
+        return productRepository.findAll();
+    }
+
+    public void addProduct(Product product){
+        productRepository.save(product);
+    }
+
+    public void removeProductById(Long id){
+        productRepository.deleteById(id);
+    }
+
+    public Optional<Product> getProductById(Long id){
+        return productRepository.findById(id);
+    }
+
+    public List<Product> getAllProductByCategory(int id){
+        return productRepository.findAllByCategory_Id(id);
+    }
+}
